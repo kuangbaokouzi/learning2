ec107cd6b8364056277e158cf8a77ddeaa8a7781
kouzikaile <35773597@qq.com>
2018-06-21 18:04:15 +0800

    add new demo

---
+++ kotlin-lecture/src/com/laowuandhisfriends/kotlin/grammer/HelloKotlin9.kt
@@ -0,0 +1,21 @@
+package com.laowuandhisfriends.kotlin.grammer
+
+import java.util.*
+import java.util.stream.Collectors
+
+fun main(args: Array<String>) {
+    var arrayList = listOf<String>("hello", "world", "welcome", "hello world", "goodbye")
+    for (item in arrayList) {
+        println(item)
+    }
+
+    println("====================")
+
+    when {
+        "world" in arrayList -> println("world in collection")
+    }
+
+    println("====================")
+
+    var list = arrayList.filter { it.length > 5 }.map { it.toUpperCase() }.sorted().forEach(System.out::println)
+}
